3085695c57b603a34b3ec503adb3eedc15172e08
William Simon Ramirez <wsimon8812@gmail.com>
2019-10-03 09:36:05 +0200

    add bootstrap to salutator project

--- austral-ng-salutator/src/app/app.module.ts
+++ austral-ng-salutator/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SalutatorComponent } from './salutator/salutator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SalutatorComponent
   ],
   imports: [
     BrowserModule
