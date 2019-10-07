05d515127bedb88c0a0eadfb79b3aba8de06e538
William Simon Ramirez <wsimon8812@gmail.com>
2019-10-07 09:51:13 +0200

    add invalid and minLength Name personal validator

--- angular-wishlist/src/app/form-destiny-travel/form-destiny-travel.component.ts
+++ angular-wishlist/src/app/form-destiny-travel/form-destiny-travel.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { DestinyTravel } from '../models/destiny-travel.model';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-form-destiny-travel',
@@ -11,11 +11,16 @@ export class FormDestinyTravelComponent implements OnInit {
 
   @Output() onItemAdded: EventEmitter<DestinyTravel>;
   fg: FormGroup;
+  minLength = 3;
 
   constructor(fb: FormBuilder) {
     this.onItemAdded = new EventEmitter();
     this.fg = fb.group({
-      name: ['', Validators.required],
+      name: ['', Validators.compose([
+        Validators.required,
+        this.nameValidator,
+        this.nameValidatorParameterizable(this.minLength)
+      ])],
       url: ['']
     });
 
@@ -31,6 +36,25 @@ export class FormDestinyTravelComponent implements OnInit {
     const d = new DestinyTravel(name, url);
     this.onItemAdded.emit(d);
     return false;
+  }
+
+  //ToDo: Use the ng LengthValidator (This is only an expample of personal validator)
+  nameValidator(control: FormControl): { [s: string]: boolean } {
+    const l = control.value.toString().trim().length;
+    if (l > 0 && l < 5) {
+      return { invalidName: true };
+    }
+    return null;
+  }
+
+  nameValidatorParameterizable(minLength: number): ValidatorFn {
+    return (control: FormControl): { [s: string]: boolean } | null => {
+      const l = control.value.toString().trim().length;
+      if (l > 0 && l < minLength) {
+        return { minNameLength: true };
+      }
+      return null;
+    }
   }
 
 }
