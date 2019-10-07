6b6517b7070e175ecd419c25fff5cad6b025a1db
William Simon Ramirez <wsimon8812@gmail.com>
2019-10-07 21:32:41 +0200

    observable to subscribe a favorite destiny travel

--- angular-wishlist/src/app/models/destinations-api-client.model.ts
+++ angular-wishlist/src/app/models/destinations-api-client.model.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import { DestinyTravel } from './destiny-travel.model';
+import { Subject, BehaviorSubject } from 'rxjs';
 
 @Injectable()
 export class DestinationsApiClient {
 
   destinations: DestinyTravel[] = [];
+  currrent: Subject<DestinyTravel> = new BehaviorSubject<DestinyTravel>(null);
 
-  constructor() { }
+  constructor() {
+    this.destinations = [];
+  }
 
   add(d: DestinyTravel) {
     this.destinations.push(d);
@@ -15,4 +19,15 @@ export class DestinationsApiClient {
   getAll(): DestinyTravel[] {
     return this.destinations;
   }
+
+  select(d: DestinyTravel) {
+    this.destinations.forEach(x => x.setSelected(false));
+    d.setSelected(true);
+    this.currrent.next(d);
+  }
+
+  suscribeOnChange(fn) {
+    this.currrent.subscribe(fn);
+  }
+
 }
