fc4ed1dbc91c13081802fb3fe107d9b733e3c35b
William Simon Ramirez <wsimon8812@gmail.com>
2019-10-09 22:47:13 +0200

    use redux to select a destiny travel as favorite

--- angular-wishlist/src/app/destiny-list/destiny-list.component.ts
+++ angular-wishlist/src/app/destiny-list/destiny-list.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { DestinyTravel } from './../models/destiny-travel.model';
-import { DestinationsApiClient } from '../models/destinations-api-client.model';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {DestinyTravel} from '../models/destiny-travel.model';
+import {DestinationsApiClient} from '../models/destinations-api-client.model';
+import {Store} from '@ngrx/store';
+import {AppState} from '../app.module';
+import {NewDestinyAction, SelectedFavoriteAction} from '../models/destinations-travel-state.model';
 
 @Component({
   selector: 'app-destiny-list',
@@ -12,14 +15,15 @@ export class DestinyListComponent implements OnInit {
   @Output() onItemAdded: EventEmitter<DestinyTravel>;
   updates: string[];
 
-  constructor(private destinationsAPIClient: DestinationsApiClient) {
+  constructor(private destinationsAPIClient: DestinationsApiClient, private store: Store<AppState>) {
     this.onItemAdded = new EventEmitter();
     this.updates = [];
-    this.destinationsAPIClient.suscribeOnChange((d: DestinyTravel) => {
-      if (d != null) {
-        this.updates.push(d.name + ' has been chosen.' )
-      }
-    });
+    this.store.select(state => state.destinations.favorite)
+      .subscribe(d => {
+        if (d != null) {
+          this.updates.push(d.name + ' has been chosen.');
+        }
+      });
   }
 
   ngOnInit() {
@@ -28,10 +32,12 @@ export class DestinyListComponent implements OnInit {
   added(d: DestinyTravel) {
     this.destinationsAPIClient.add(d);
     this.onItemAdded.emit(d);
+    this.store.dispatch(new NewDestinyAction((d)));
   }
 
   selected(e: DestinyTravel) {
     this.destinationsAPIClient.select(e);
+    this.store.dispatch(new SelectedFavoriteAction((e)));
   }
 
 }
