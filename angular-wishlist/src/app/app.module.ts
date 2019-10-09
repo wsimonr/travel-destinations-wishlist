51474dca50ec180f9f310ef0dd529de742049078
William Simon Ramirez <wsimon8812@gmail.com>
2019-10-09 15:48:14 +0200

    add destinations-travel-state.model implementation in app module

--- angular-wishlist/src/app/app.module.ts
+++ angular-wishlist/src/app/app.module.ts
@@ -1,20 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import { DestinyTravelComponent } from './destiny-travel/destiny-travel.component';
-import { DestinyListComponent } from './destiny-list/destiny-list.component';
-import { DestinyDetailComponent } from './destiny-detail/destiny-detail.component';
-import { DestinationsApiClient} from './models/destinations-api-client.model';
-import { FormDestinyTravelComponent } from './form-destiny-travel/form-destiny-travel.component';
+import {ActionReducerMap, StoreModule as NgRxStoreModule} from '@ngrx/store';
+import {EffectsModule} from '@ngrx/effects';
+import {AppComponent} from './app.component';
+import {DestinyTravelComponent} from './destiny-travel/destiny-travel.component';
+import {DestinyListComponent} from './destiny-list/destiny-list.component';
+import {DestinyDetailComponent} from './destiny-detail/destiny-detail.component';
+import {DestinationsApiClient} from './models/destinations-api-client.model';
+import {FormDestinyTravelComponent} from './form-destiny-travel/form-destiny-travel.component';
+import {
+  DestinationsTravelEffects,
+  DestinationsTravelState,
+  initializeDestinationsTravelState,
+  reducerDestinationsTravel
+} from './models/destinations-travel-state.model';
 
 const routes: Routes = [
-  {path:'', redirectTo:'home', pathMatch: 'full'},
-  {path:'home', component: DestinyListComponent},
-  {path:'destiny', component: DestinyDetailComponent}
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: DestinyListComponent},
+  {path: 'destiny', component: DestinyDetailComponent}
 ];
+
+// redux init
+export interface AppState {
+  destinations: DestinationsTravelState;
+}
+
+const reducers: ActionReducerMap<AppState> = {
+  destinations: reducerDestinationsTravel
+};
+
+const reducersInitialState = {
+  destinations: initializeDestinationsTravelState()
+};
+
+// redux fin init
 
 @NgModule({
   declarations: [
@@ -22,17 +44,20 @@ const routes: Routes = [
     DestinyTravelComponent,
     DestinyListComponent,
     DestinyDetailComponent,
-    FormDestinyTravelComponent
+    FormDestinyTravelComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    NgRxStoreModule.forRoot(reducers, {initialState: reducersInitialState}),
+    EffectsModule.forRoot([DestinationsTravelEffects])
   ],
   providers: [
     DestinationsApiClient
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
