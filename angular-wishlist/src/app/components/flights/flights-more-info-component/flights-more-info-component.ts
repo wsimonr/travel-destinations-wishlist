e48b5b8d509237ec2ff296441a7b1b69352c5358
William Simon Ramirez <wsimon8812@gmail.com>
2019-10-11 15:46:47 -0400

    feat(flight-more-info): more info of travel destiny's flight

---
+++ angular-wishlist/src/app/components/flights/flights-more-info-component/flights-more-info-component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flights-more-info-component',
+  templateUrl: './flights-more-info-component.html',
+  styleUrls: ['./flights-more-info-component.css']
+})
+export class FlightsMoreInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
