40eb4e29eedbe58bc7a34f26a11365cf71eda0d3
William Simon Ramirez <wsimon8812@gmail.com>
2019-10-11 15:43:19 -0400

    feat(flight-main): main travel destiny flight

---
+++ angular-wishlist/src/app/components/flights/flights-main-component/flights-main-component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flights-main-component',
+  templateUrl: './flights-main-component.html',
+  styleUrls: ['./flights-main-component.css']
+})
+export class FlightsMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
