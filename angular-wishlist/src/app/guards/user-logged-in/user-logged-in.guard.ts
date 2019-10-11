4bf2a0e9ea38cba961fb7edfffb4571d90c43ae8
William Simon Ramirez <wsimon8812@gmail.com>
2019-10-11 10:45:05 -0400

    perf(login-guard): guard to verify whether a user is logged in or not

---
+++ angular-wishlist/src/app/guards/user-logged-in/user-logged-in.guard.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from 'rxjs';
+import {AuthService} from '../../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserLoggedInGuard implements CanActivate {
+  constructor(private authService: AuthService) {
+  }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const isLoggedIn = this.authService.isLoggedIn();
+    console.log('canActivate', isLoggedIn);
+    return isLoggedIn;
+  }
+}
